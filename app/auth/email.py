2f12f8a29fa44e7db45b31e044011dab0b3cedf6
mtucci <tucci.michele@gmail.com>
2022-07-07 12:43:55 +0200

    Config for the mail From: field

--- app/auth/email.py
+++ app/auth/email.py
@@ -13,7 +13,7 @@ def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
         "[AnnotateChange] Reset your password",
-        sender=current_app.config["ADMINS"][0],
+        sender=current_app.config["MAIL_FROM"],
         recipients=[user.email],
         text_body=render_template(
             "email/reset_password.txt", user=user, token=token
@@ -28,7 +28,7 @@ def send_email_confirmation_email(user):
     token = user.get_email_confirmation_token()
     send_email(
         "[AnnotateChange] Confirm your email",
-        sender=current_app.config["ADMINS"][0],
+        sender=current_app.config["MAIL_FROM"],
         recipients=[user.email],
         text_body=render_template(
             "email/confirm_email.txt", user=user, token=token
--- config.py
+++ config.py
@@ -41,6 +41,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_FROM = os.environ.get("MAIL_FROM")
     ADMINS = [
         x.strip()
         for x in os.environ.get("ADMIN_EMAILS", "").split(";")
